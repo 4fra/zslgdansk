2d74abd9c8331f2201e3a5dca4f55c41bd6dab8c
4o4fra <franio.grzywaczewski@gmail.com>
2025-01-04 23:06:34 +0100

    :lipstick: Color changes

--- src/components/GradientCircularProgressbar/GradientCircularProgressbar.tsx
+++ src/components/GradientCircularProgressbar/GradientCircularProgressbar.tsx
@@ -3,8 +3,10 @@ import React from 'react';
 import {buildStyles, CircularProgressbar} from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import {GradientCircularProgressbarProps} from './types/GradientCircularProgressbarProps';
+import {useMantineTheme} from '@mantine/core';
 
 const GradientCircularProgressbar: React.FC<GradientCircularProgressbarProps> = ({value, text}) => {
+    const theme = useMantineTheme();
     const isNegative = value < 0;
     const displayValue = Math.abs(value);
 
@@ -13,8 +15,8 @@ const GradientCircularProgressbar: React.FC<GradientCircularProgressbarProps> = 
             <svg style={{height: 0}}>
                 <defs>
                     <linearGradient id="gradientColors" gradientTransform="rotate(90)">
-                        <stop offset="0%" stopColor="#3e98c7"/>
-                        <stop offset="100%" stopColor="#f88"/>
+                        <stop offset="0%" stopColor={theme.colors.red[5]}/>
+                        <stop offset="100%" stopColor={theme.colors.red[6]}/>
                     </linearGradient>
                 </defs>
             </svg>
@@ -25,9 +27,9 @@ const GradientCircularProgressbar: React.FC<GradientCircularProgressbarProps> = 
                 styles={buildStyles({
                     textSize: '16px',
                     pathColor: 'url(#gradientColors)',
-                    textColor: '#f88',
-                    trailColor: '#d6d6d6',
-                    backgroundColor: '#3e98c7',
+                    textColor: theme.colors.red[6],
+                    trailColor: theme.colors.gray[4],
+                    backgroundColor: theme.colors.blue[6],
                 })}
             />
         </div>
