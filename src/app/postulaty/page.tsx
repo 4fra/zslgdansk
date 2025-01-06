d00c1b5bf32374962edd3bd121142ae3d419eec5
4o4fra <franio.grzywaczewski@gmail.com>
2025-01-06 20:48:09 +0100

    :lipstick: Change title of full postulates page

--- src/app/postulaty/page.tsx
+++ src/app/postulaty/page.tsx
@@ -1,4 +1,4 @@
-import {Container, Title} from "@mantine/core";
+import {Container, Text, Title} from "@mantine/core";
 import {postulates} from "@/app/postulaty/data/postulates";
 import Postulate from "@/components/Postulate/Postulate";
 import classes from './page.module.css';
@@ -6,7 +6,8 @@ import classes from './page.module.css';
 export default function Page() {
     return (
         <Container className={classes.inner}>
-            <Title mb={30}>Moje postulaty</Title>
+            <Title>Pełne postulaty</Title>
+            <Text mb={30}>Franciszka Grzywaczewskiego, kandydata na przewodniczącego SU ZSŁ w Gdańsku</Text>
             {postulates.map((postulate, index) => (
                 <Postulate key={index} title={postulate.title} description={postulate.description}/>
             ))}
