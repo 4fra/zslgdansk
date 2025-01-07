320ba5a10deccd752aff82ab7e1ce3ae8c062069
4o4fra <franio.grzywaczewski@gmail.com>
2025-01-07 22:22:15 +0100

    :sparkles: Redirect to home page after clicking the logo

--- src/components/PlutaLogo/PlutaLogo.tsx
+++ src/components/PlutaLogo/PlutaLogo.tsx
@@ -1,7 +1,24 @@
 import {Image} from '@mantine/core';
+import {useRouter} from 'next/navigation';
 
-const PlutaLogo = ({h = 48}: { h?: number }) => (
-    <Image className={"logoContainer"} src="/domofon.png" alt="ZSL Logo" w={"auto"} h={h}/>
-);
+const PlutaLogo = ({h = 48}: { h?: number }) => {
+    const router = useRouter();
+
+    const handleClick = () => {
+        router.push('/');
+    };
+
+    return (
+        <Image
+            className={"logoContainer"}
+            src="/domofon.png"
+            alt="ZSL Logo"
+            w={"auto"}
+            h={h}
+            onClick={handleClick}
+            style={{cursor: 'pointer'}}
+        />
+    );
+};
 
 export default PlutaLogo;
